69cb81edb8666f07f00d5bc15619288a94c2f7f1
unknown <lash.nkl@gmail.com>
2024-09-28 23:18:34 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ const connectDatabase = ()=>{
     mongoose.connect(process.env.DB_LOCAL_URI,{
         useNewUrlParser:true,
         useUnifiedTopology:true
-    }).then(con=>{
+    }).then(con=>{ 
         console.log(`MongoDB is connected to the host: ${con.connection.host} `)
     })  
-}
+} 
 
 module.exports = connectDatabase;    
